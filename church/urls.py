1c52144a0ca9e3040b6c559359d21ee43b745a80
lennykioko <lennykmutua@gmail.com>
2019-02-28 12:20:35 +0300

    Update admin site header

--- church/urls.py
+++ church/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+admin.site.site_header = 'CHURCH MANAGEMENT'
+admin.site.site_title = 'CHURCH MANAGEMENT'
+
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
